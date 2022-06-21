db9fd4262c20ac55f1e0c0893ef2e15ef0363f8e
Lionel <92838415+LC-GHub@users.noreply.github.com>
2022-06-21 18:29:42 +0800

    Add files via upload

---
+++ facial_req.py
@@ -0,0 +1,165 @@
+#! /usr/bin/python
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
+import RPi.GPIO as GPIO
+
+#Initialize Motor Pins
+in1 = 17
+in2 = 18
+in3 = 27
+in4 = 22
+#Initialize motor steps and sequence
+step_sleep = 0.002
+step_count = 4096 #360deg
+direction = False #True is clockwise and False is Anti_CW
+
+step_seq =[
+    [1,0,0,1],
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1]
+]
+
+
+
+#cleanup function the motor
+def cleanup():
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+    GPIO.cleanup()
+
+#Initialize 'currentname' to trigger only when a new person is identified.
+currentname = "unknown"
+#Determine faces from encodings.pickle file model created from train_model.py
+encodingsP = "encodings.pickle"
+
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(encodingsP, "rb").read())
+
+# initialize the video stream and allow the camera sensor to warm up
+# Set the ser to the followng
+# src = 0 : for the build in single web cam, could be your laptop webcam
+# src = 2 : I had to set it to 2 inorder to use the USB webcam attached to my laptop
+#vs = VideoStream(src=2,framerate=10).start()
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+# start the FPS counter
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	# Detect the fce boxes
+	boxes = face_recognition.face_locations(frame)
+	# compute the facial embeddings for each face bounding box
+	encodings = face_recognition.face_encodings(frame, boxes)
+	names = []
+
+	# loop over the facial embeddings
+	for encoding in encodings:
+		# attempt to match each face in the input image to our known
+		# encodings
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+		name = "Unknown" #if face is not recognized, then print Unknown
+
+		# check to see if we have found a match
+		if True in matches:
+			# find the indexes of all matched faces then initialize a
+			# dictionary to count the total number of times each face
+			# was matched
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# loop over the matched indexes and maintain a count for
+			# each recognized face face
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# determine the recognized face with the largest number
+			# of votes (note: in the event of an unlikely tie Python
+			# will select first entry in the dictionary)
+			name = max(counts, key=counts.get)
+
+			#If someone in your dataset is identified, print their name on the screen
+			if currentname != name:
+				currentname = name
+				print(currentname)
+				######activate the motor#####################
+				#setting up the motor
+				GPIO.setmode( GPIO.BCM )
+				GPIO.setup( in1, GPIO.OUT )
+				GPIO.setup( in2, GPIO.OUT )
+				GPIO.setup( in3, GPIO.OUT )
+				GPIO.setup( in4, GPIO.OUT )
+				#initializing the movement
+				GPIO.output( in1, GPIO.LOW )
+				GPIO.output( in2, GPIO.LOW )
+				GPIO.output( in3, GPIO.LOW )
+				GPIO.output( in4, GPIO.LOW )
+				motor_pins = [in1, in2, in3, in4]
+				
+				motor_step_counter = 0 #init to 0
+				i = 0
+				for i in range(step_count):
+					for pin in range(0, len(motor_pins)):
+						GPIO.output(motor_pins[pin], step_seq[motor_step_counter][pin])
+					motor_step_counter = (motor_step_counter - 1)% 8
+					time.sleep(step_sleep)
+				cleanup()
+				#cv2.destroyAllWindows()
+				#vs.stop()
+
+				
+
+		# update the list of names
+		names.append(name)
+
+	# loop over the recognized faces
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# draw the predicted face name on the image - color is in BGR
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 225), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			.8, (0, 255, 255), 2)
+
+	# display the image to our screen
+	cv2.imshow("Facial Recognition is Running", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# quit when 'q' key is pressed
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
